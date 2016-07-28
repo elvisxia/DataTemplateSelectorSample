7bf208755df80661496cf02f5e52863935229b84
v-xucxia <v-xucxia@PC-ELVISXIA>
2016-07-28 17:22:18 +0800

    Add project files.

---
+++ DataTemplateSelectorSample/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using DataTemplateSelectorSample.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DataTemplateSelectorSample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            List<Customer> list = new List<Customer>();
+            for (int i = 0; i < 20; i++)
+            {
+                Customer newCustomer = new Customer();
+                if (i ==12)
+                {
+                    newCustomer.Id = "VipCustomer";
+                }
+                else
+                {
+                    newCustomer.Id = "CustomerId" + i;
+                }
+                newCustomer.Name = "Customer" + i;
+                list.Add(newCustomer);
+            }
+            myListView.ItemsSource = list;
+            base.OnNavigatedTo(e);
+        }
+    }
+}
---
+++ DataTemplateSelectorSample/TemplateSelectors/MyTemplateSelector.cs
@@ -0,0 +1,31 @@
+﻿using DataTemplateSelectorSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DataTemplateSelectorSample.TemplateSelectors
+{
+    public class MyTemplateSelector: DataTemplateSelector
+    {
+        public DataTemplate VIPTemplate { get; set; }//DataTemplate for special user
+        public DataTemplate CustomerTemplate { get; set; }//DataTemplate for normal user
+        protected override DataTemplate SelectTemplateCore(object item)
+        {
+            if (((Customer)item).Id.ToLower() == "vipcustomer")
+            {
+                //if current user is vip then user VIP Template.
+                return VIPTemplate;
+            }
+            else
+            {
+                //otherwise return normal template.
+                return CustomerTemplate;
+            }
+            
+        }
+    }
+}
